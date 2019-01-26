3c6c3ea88caf86917a5de610a2d5e815df257836
lintangwisesa <lintangwisesa@ymail.com>
2019-01-27 00:48:27 +0700

    dci frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react'
 import Dashboard from './dci_Dashboard'
+import axios from 'axios'
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
 
 class App extends Component {
   
   state = {
-    login: false
+    login: false,
+    loading: false,
+    user: {}
+  }
+
+  createNotification = (type) => {
+    return () => {
+      switch (type) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'loginOK':
+          NotificationManager.success(`Selamat datang ${this.state.user.nama}`, 'Selamat datang!');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+          break;
+        case 'kosong':
+          NotificationManager.warning('Ketik nama & password Anda untuk proses autentikasi', 'Input nama & password!');
+          break;
+        case 'gagal':
+          NotificationManager.error('Gagal! nama & password Anda tidak terdaftar', 'Login gagal!');
+          break;
+      }
+    };
+  };
+
+  masukLogin = ()=>{
+    this.setState({
+      loading: true
+    })
+    if (this.refs.nama.value || this.refs.password.value){
+      var url = 'http://localhost:1234/login'
+      axios.post(url, {
+        nama: this.refs.nama.value,
+        password: this.refs.password.value
+      }).then((res)=>{
+        console.log(res.data.status)
+        if(res.data.status === 'error'){
+          setTimeout(
+            this.createNotification('gagal'), 500
+          )
+          this.setState({loading: false})
+          this.refs.nama.value = ''
+          this.refs.password.value = ''
+        } else {
+          this.setState({
+            login: true,
+            user: res.data
+          })
+        }
+      }).catch((res)=>{
+        console.log(res.data.status)
+      })
+    } else {
+      setTimeout(
+        this.createNotification('kosong'), 500
+      )
+      this.setState({loading: false})
+    }
   }
 
   render(){
-    
+
     // form login
     var loginForm = 
     <div>
@@ -21,45 +83,46 @@ class App extends Component {
         </div>
         <div class="card card-login mx-auto">
           <h5 class="card-header bg-dark text-white text-center">
-            Login ke Dashboard
+            {this.state.loading ? <img style={{height:'30px', width:'30px'}} alt="" src="wait.gif"/> : 'Login ke Dashboard'}
           </h5>
           <div class="card-body">
             <form>
               <div class="form-group">
                 <div class="form-label-group">
-                  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus"/>
+                  <input ref='nama' type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus"/>
                   <label for="inputEmail">Nama pengguna</label>
                 </div>
               </div>
               <div class="form-group">
                 <div class="form-label-group">
-                  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required"/>
+                  <input ref='password' type="password" id="inputPassword" class="form-control" placeholder="Password" required="required"/>
                   <label for="inputPassword">Password</label>
                 </div>
               </div>
               <div class="form-group">
               </div>
-                <a onClick={()=>{this.setState({login: true})}}
+                <a onClick={this.masukLogin}
                 class="btn btn-success btn-block text-white" href>
-                  Login
+                  {this.state.loading ? <img style={{height:'30px', width:'30px'}} alt="" src="wait.gif"/> : <span><i class="fas fa-sign-in-alt"></i>&nbsp;&nbsp;Login</span>}
                 </a>
             </form>
             <div class="text-center">
               <small class="d-block small my-3">
-                Copyright © PT Dela Cemara Indah 2018
+                Copyright © PT Dela Cemara Indah 2019
               </small>
             </div>
           </div>
         </div>
       </div>
         <small className="text-center" style={{position: 'fixed', bottom: '25px', width:'100%'}}>
-          Copyright © PT Dela Cemara Indah 2018
+          Copyright © PT Dela Cemara Indah 2019
         </small>
     </div>
     
     return(
       <div>
-        {this.state.login ? <Dashboard/> : loginForm}
+        <NotificationContainer/>
+        {this.state.login ? <Dashboard user={this.state.user}/> : loginForm}
       </div>
     )
   }
--- src/dci_Dashboard.js
+++ src/dci_Dashboard.js
@@ -1,10 +1,100 @@
 import React, { Component } from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+import axios from 'axios';
 
 class Dashboard extends Component {
+
+  state = {
+    user: this.props.user,
+    subMaster: [],
+    subMutasi: [],
+    subLaporan: []
+  }
+
+  createNotification = (type) => {
+    return () => {
+      switch (type) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'loginOK':
+          NotificationManager.success(`Selamat datang ${this.state.user.nama}`, 'Selamat datang!');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+          break;
+        case 'kosong':
+          NotificationManager.warning('Ketik nama & password Anda untuk proses autentikasi', 'Input nama & password!');
+          break;
+        case 'gagal':
+          NotificationManager.error('Gagal! nama & password Anda tidak terdaftar', 'Login gagal!');
+          break;
+      }
+    };
+  };
+
+  componentDidMount(){
+    this.setState({
+      user: this.props.user
+    })
+    console.log(this.state.user)
+    setTimeout(
+      this.createNotification('loginOK'), 500
+    )
+    axios.post('http://localhost:1234/menu/master', {
+      nama: this.state.user.nama
+    }).then((x)=>{
+      console.log(x.data)
+      if(x.data.length > 0){
+        this.setState({subMaster: x.data})
+      } else {
+        this.setState({subMaster: [{submenu: 'Menu tidak tersedia'}]})
+      }
+    })
+    axios.post('http://localhost:1234/menu/mutasi', {
+      nama: this.state.user.nama
+    }).then((x)=>{
+      console.log(x.data)
+      if(x.data.length > 0){
+        this.setState({subMutasi: x.data})
+      } else {
+        this.setState({subMutasi: [{submenu: 'Menu tidak tersedia'}]})
+      }
+    })
+    axios.post('http://localhost:1234/menu/laporan', {
+      nama: this.state.user.nama
+    }).then((x)=>{
+      console.log(x.data)
+      if(x.data.length > 0){
+        this.setState({subLaporan: x.data})
+      } else {
+        this.setState({subLaporan: [{submenu: 'Menu tidak tersedia'}]})
+        console.log(this.state.subLaporan)
+      }
+    })
+  }
+
   render() {
+
+    var subMaster = this.state.subMaster.map((val, i)=>{
+      var menu = val.submenu
+      return <a key={i} class="dropdown-item" style={{cursor:'pointer'}}>{menu}</a>
+    })
+
+    var subMutasi = this.state.subMutasi.map((val, i)=>{
+      var menu = val.submenu
+      return <a key={i} class="dropdown-item" style={{cursor:'pointer'}}>{menu}</a>
+    })
+
+    var subLaporan = this.state.subLaporan.map((val, i)=>{
+      var menu = val.submenu
+      return <a key={i} class="dropdown-item" style={{cursor:'pointer'}}>{menu}</a>
+    })
+
     return (
       <div className="Dashboard">
-        
+        <NotificationContainer/>
         <nav style={{position: 'fixed', width:'100%', zIndex:'9999', height:'70px'}} class="navbar navbar-expand navbar-dark bg-dark static-top">
 
           <a class="navbar-brand mr-1" href="/">
@@ -18,12 +108,42 @@ class Dashboard extends Component {
             <li class="nav-item dropdown no-arrow mx-1">
               <a class="nav-link dropdown-toggle" href="" id="alertsDropdown" role="button" 
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Menu 1
+                Master File
               </a>
               <div class="dropdown-menu dropdown-menu-left" aria-labelledby="alertsDropdown">
-                <a class="dropdown-item" href="">Action</a>
-                <a class="dropdown-item" href="">Another action</a>
-                <a class="dropdown-item" href="">Something else here</a>
+                {subMaster}
+              </div>
+            </li>
+
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link dropdown-toggle" href="" id="alertsDropdown" role="button" 
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Mutasi
+              </a>
+              <div class="dropdown-menu dropdown-menu-left" aria-labelledby="alertsDropdown">
+                {subMutasi}
+              </div>
+            </li>
+
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link dropdown-toggle" href="" id="alertsDropdown" role="button" 
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Laporan
+              </a>
+              <div class="dropdown-menu dropdown-menu-left" aria-labelledby="alertsDropdown">
+                {subLaporan}
+              </div>
+            </li>
+
+            <li class="nav-item dropdown no-arrow ml-auto">
+              <a class="nav-link dropdown-toggle" href="" id="alertsDropdown" role="button" 
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>&nbsp;&nbsp;
+                {this.state.user.nama}
+              </a>
+              <div class="dropdown-menu dropdown-menu-left" aria-labelledby="alertsDropdown">
+                <a class="dropdown-item" style={{cursor:'pointer'}}>Profil Saya</a>
+                <a class="dropdown-item" style={{cursor:'pointer'}}>Logout</a>
               </div>
             </li>
 
